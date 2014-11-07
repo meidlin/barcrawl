a03df8793e0d7ebea04aea5d407b0d963759aaad
meidlin <mikeeidlin@gmail.com>
2014-11-06 17:39:00 -0800

    Broken but almost working gmaps

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,13 @@ class Event
   include Mongoid::Document
   field :title, type: String
   field :date, type: String
+  field :time, type: String
   field :description, type: String
-  field :location, type: String
+  field :start, type: String
+  field :bar1, type: String
+  field :bar2, type: String
+  field :bar3, type: String
+  field :bar4, type: String
+  field :afterparty, type: String
 end
 
